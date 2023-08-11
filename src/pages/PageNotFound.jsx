46a6ae1fa094648c4a980963861007a40fd3304e
ArKanZi <ayushku.ak@gmail.com>
2023-08-11 18:38:59 +0530

    custom colors

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,6 +1,6 @@
 const PageNotFound = () => {
     document.title = "404 Page Not Found"
-    return ( <div className="w-screen h-screen bg-bluegray-700 text-bluegray-300 flex items-center justify-center text-7xl text-center font-bold">
+    return ( <div className="w-screen h-screen flex items-center justify-center text-7xl text-center font-bold bg-primary-x900">
         <div>404 Page Not Found</div>
     </div> );
 }
