104f2be9ff9a1525dc8057ac3f0aa6437fdda0be
ArKanZi <ayushku.ak@gmail.com>
2023-08-11 19:43:18 +0530

    navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
 import LeftSidebar from "../components/LeftSidebar";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
     document.title = "JustSmile | Home"
-    return ( <div className="flex h-screen w-full max-w-[120rem] mx-auto">
-        <div className="flex-[0_1_20%] min-w-[24rem]">
+    return (<>
+        <div><Navbar /></div>
+    <div className="flex h-screen w-full max-w-[120rem] mx-auto">
+        <div className="flex h-screen w-full max-w-[120rem] mx-auto">
+            <div className="flex-[0_1_20%] min-w-[24rem]">
             <LeftSidebar />
+            </div>
+            <div className="flex-1 bg-red-500"></div>
+            <div className="flex-[0_1_20%] bg-green-500 min-w-[24rem]"></div>
         </div>
-        <div className="flex-1 bg-red-500"></div>
-        <div className="flex-[0_1_20%] bg-green-500 min-w-[24rem]"></div>
-    </div> );
+    </div> </>);
+
 }
  
 export default Home;
