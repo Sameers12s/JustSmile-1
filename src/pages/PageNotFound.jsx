365d28ed51766c683e0cf1a65ce3b6d75e5e4c8c
ArKanZi <ayushku.ak@gmail.com>
2023-08-13 11:20:55 +0530

    first commit

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,6 +1,6 @@
 const PageNotFound = () => {
     document.title = "404 Page Not Found"
-    return ( <div className="w-screen h-screen flex items-center justify-center text-7xl text-center font-bold bg-primary-x900">
+    return ( <div className="w-screen h-screen flex items-center justify-center text-7xl text-center font-bold bg-primary-x900 ">
         <div>404 Page Not Found</div>
     </div> );
 }
