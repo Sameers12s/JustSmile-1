98e60c9cc2eb7d1e26fbe4203431796ff49ecffe
ArKanZi <ayushku.ak@gmail.com>
2023-08-20 23:24:38 +0530

    security update

--- src/components/Security.jsx
+++ src/components/Security.jsx
@@ -2,8 +2,8 @@ const Security = () => {
   return (
     <div className="flex h-full w-full items-center justify-center">
       <div className="flex flex-col h-auto w-full justify-between bg-neutrals-800 rounded-lg">
-        <div className="flex flex-row px-3 pt-4">
-          <h1 className=" basis-1/4 mt-3 mr-4">Change Email</h1>
+        <div className="flex flex-row px-3 pt-4 pb-2">
+          <h1 className=" basis-1/4 mr-4 mt-2">Change Email</h1>
           <div className="basis-3/4">
             <input
               id="email"
@@ -13,13 +13,13 @@ const Security = () => {
           </div>
         </div>
         <div className="flex flex-row px-3 pb-4">
-          <h1 className="basis-1/4 mt-3 mr-4">Change Password</h1>
+          <h1 className="basis-1/4 mt-4 mr-4">Change Password</h1>
           <div className="basis-3/4">
             <button
               type="button"
               className="btn w-full text-neutrals-500 bg-neutrals-700 mt-2"
             >
-              MANAGE
+              SEND PASSWORD RESET LINK
             </button>
           </div>
         </div>
