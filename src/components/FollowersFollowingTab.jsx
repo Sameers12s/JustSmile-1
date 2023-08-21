ca368c757ee8143f7f339b26b243e2e6de5c74bf
ArKanZi <ayushku.ak@gmail.com>
2023-08-21 22:10:19 +0530

    fix

--- src/components/FollowersFollowingTab.jsx
+++ src/components/FollowersFollowingTab.jsx
@@ -3,7 +3,7 @@ const FollowersFollowingTab = ({ children }) => {
     <div>
       <div className="flex flex-row h-36 rounded-lg justify-center items-center text-center bg-neutrals-800 font-bold">
         <div className="stats bg-neutrals-800">
-          <div className="stat p-2 place-items-center">
+          <div className="stat px-2 place-items-center">
             <div className="stat-figure"></div>
             <div className="stat-value text-xl">100K</div>
             <div className="stat-desc">FOLLOWING</div>
--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -2,7 +2,7 @@ import FollowersFollowingTab from "./FollowersFollowingTab";
 import FriendsTab from "./FriendsTab";
 const RightSidebar = () => {
   return (
-    <div className="flex flex-col h-full px-4 pb-4 space-y-4">
+    <div className="flex flex-col h-full p-4 py-4 space-y-4">
       <FollowersFollowingTab />
       <FriendsTab />
     </div>
