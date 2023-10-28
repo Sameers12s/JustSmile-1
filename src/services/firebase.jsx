f4b7f0e7b06a999115cd68ffff0d396d58d93b26
Sameers12s <samiboy158@gmail.com>
2023-10-28 21:02:47 +0530

    changes

--- src/services/firebase.jsx
+++ src/services/firebase.jsx
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-VJhSZKKC4hejmeQI_Zd84A8x8sfDfQA",
-  authDomain: "just-smiles-0.firebaseapp.com",
-  projectId: "just-smiles-0",
-  storageBucket: "just-smiles-0.appspot.com",
-  messagingSenderId: "935821144659",
-  appId: "1:935821144659:web:fcce3f578594237bca8ca6",
-  measurementId: "G-PGW9PZN1C8",
+  apiKey: "AIzaSyC2fBRLXgq8-cMcOPWjR234vTeOtXszFV4",
+  authDomain: "just-smiles-12.firebaseapp.com",
+  projectId: "just-smiles-12",
+  storageBucket: "just-smiles-12.appspot.com",
+  messagingSenderId: "32627769695",
+  appId: "1:32627769695:web:cd691232da10dc769f36ea",
+  measurementId: "G-ZK17Y7156N"
 };
 
 // Initialize Firebase
